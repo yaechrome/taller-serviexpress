ce3decdb68cf46701e2b0aac29ac825725e66555
Joselyn Moya <joselyn.moyap@gmail.com>
2018-05-27 17:06:21 -0400

    Se agrega Services y Services Impl

---
+++ taller-serviexpress-services-impl/src/main/java/cl/taller/serviexpress/services/impl/PerfilServicesImpl.java
@@ -0,0 +1,17 @@
+
+package cl.taller.serviexpress.services.impl;
+
+import cl.taller.serviexpress.domain.Perfil;
+import cl.taller.serviexpress.dao.hibernate.PerfilDaoHibernate;
+import cl.taller.serviexpress.dao.PerfilDao;
+import cl.taller.serviexpress.services.PerfilServices;
+import java.util.List;
+public class PerfilServicesImpl implements PerfilServices{
+
+    @Override
+    public List<Perfil> listarPerfiles() {
+        PerfilDaoHibernate dao = new PerfilDaoHibernate();
+        return dao.findAll();
+    }
+    
+}
